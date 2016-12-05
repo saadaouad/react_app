6176929ec6cc4464af6fd708620ee8a9f362dbc9
saadaouad <aouadsaad05@gmail.com>
2016-12-05 12:03:24 +0000

    Integrate Mock API

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('app')
 );
