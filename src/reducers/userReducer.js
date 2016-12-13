97fa5f8115a004c46676b5e3dae032f14b113681
saadaouad <aouadsaad05@gmail.com>
2016-12-13 10:45:00 +0000

    Create reducer for load users

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,10 +2,8 @@ import * as types from '../actions/actionTypes';
 
 export default function userReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_USER:
-      return [...state,
-        Object.assign({}, action.user)
-      ];
+    case types.LOAD_USERS_SUCCESS:
+      return action.users;
     default:
       return state;
   }
