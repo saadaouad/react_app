6b2a4400fc3b0076cbbe11e35f10733fe8b48afe
saadaouad <aouadsaad05@gmail.com>
2016-12-13 23:12:51 +0000

    Manage user form

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react',
+import React, { PropTypes } from 'react';
 
 const TextInput = ({name, label, onChange, placeholder, value, error}) => {
   let wrapperClass = 'form-group';
--- src/components/user/ManageUserPage.js
+++ src/components/user/ManageUserPage.js
@@ -2,25 +2,35 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as userAction from '../../actions/userAction';
+import UserForm from './UserForm';
 
 class ManageUserPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      user: Object.assign({}, this.props.user),
+      errors: {}
+    };
   }
   render() {
     return(
-      <h1>Manage User</h1>
+        <UserForm
+          user={this.state.user}
+          errors={this.state.errors}
+        />
     );
   }
 }
 
 ManageUserPage.PropTypes = {
-  //myProp : PropTypes.string.isRequired
+  myProp : PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+  let user = {id: '', firstName: '', lastName: '', profile: ''};
   return {
-    state: state
+    user: user
   };
 }
 
