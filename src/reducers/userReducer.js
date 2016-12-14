80cd56ab21693d4c5da7e9914458ae1ae1d5b999
saadaouad <aouadsaad05@gmail.com>
2016-12-14 11:32:28 +0000

    Pass action create and update user in reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,6 +4,19 @@ export default function userReducer(state = [], action) {
   switch(action.type) {
     case types.LOAD_USERS_SUCCESS:
       return action.users;
+
+    case types.CREATE_USER_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.user)
+      ];
+
+    case types.UPDATE_USER_SUCCESS:
+      return [
+        ...state.filter(user => user.id !== action.user.id),
+        Object.assign({}, action.user)
+      ];
+      
     default:
       return state;
   }
