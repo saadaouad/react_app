ad011ef5634f33dedfd4570c75d427bef3abc6c2
saadaouad <aouadsaad05@gmail.com>
2016-12-29 00:59:08 +0000

    test rebase

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
