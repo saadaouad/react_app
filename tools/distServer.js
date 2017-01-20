b111a8ae178ba1bacc8e98e4b09928cf0f181250
saadaouad <aouadsaad05@gmail.com>
2017-01-20 11:10:57 +0000

    Add compressor module to bundle file

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import compression from 'compression';
 
 const port = 3000;
 const app = express();
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
