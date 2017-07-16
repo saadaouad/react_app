1a2b859fce0167b4ed9e2797140948e5c47c0ba6
saadaouad <aouadsaad05@gmail.com>
2017-07-16 01:38:48 +0100

    Add deploy.sh

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+var ghpages = require('gh-pages');
+
+module.exports = {
+  entry : './src/index.js',
+  output : {
+    filename: 'public/js/index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 9000,
+    historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /(\.css)$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('public/styles/main.css', {
+      allChunks: true
+    })
+  ]
+};
+
+ghpages.publish('dist', {
+  branch: 'master',
+  repo: 'https://github.com/saadaouad/react_app.git'
+}, function(err) {});
