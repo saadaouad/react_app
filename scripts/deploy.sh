5b9ed34fae35c135980f4c520ddcd8ae262a5fd6
saadaouad <aouadsaad05@gmail.com>
2017-07-23 20:05:43 +0100

    Add url repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
+GIT_DEPLOY_REPO=${https://github.com/saadaouad/react_app/:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
 
 cd dist && \
 $(npm bin)/rimraf .git
 git init && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
-git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
+git push --force "${https://github.com/saadaouad/react_app/}" master:gh-pages
 
 # ! #
 # IF YOU USE A USERNAME.GITHUB.IO ROOT DOMAIN, PLEASE READ THE WARNING BELOW
