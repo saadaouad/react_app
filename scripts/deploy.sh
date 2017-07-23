1793d813905c3b002e32157daf89476d4a4efe1a
saadaouad <aouadsaad05@gmail.com>
2017-07-23 20:10:34 +0100

    Fix url of git deploy repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-https://github.com/saadaouad/react_app/=${https://github.com/saadaouad/react_app/:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
+GIT_DEPLOY_REPO=${https://github.com/saadaouad/react_app/blob/master/package.json}
 
 cd dist && \
 $(npm bin)/rimraf .git
 git init && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
-git push --force "${https://github.com/saadaouad/react_app/}" master:gh-pages
+git push --force "${https://github.com/saadaouad/react_app}" master:gh-pages
 
 # ! #
 # IF YOU USE A USERNAME.GITHUB.IO ROOT DOMAIN, PLEASE READ THE WARNING BELOW
