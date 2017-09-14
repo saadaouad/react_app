33d94367ffae35476eeb1fcc9d2df1aba1a68d19
saadaouad <aouadsaad05@gmail.com>
2017-09-14 01:20:48 +0100

    Use props-types

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from './common/Header';
 
 class App extends React.Component {
--- src/components/user/UserForm.js
+++ src/components/user/UserForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TextInput from '../common/TextInput';
 
 const UserForm = ({user, onSave, onChange, loading, errors}) => {
